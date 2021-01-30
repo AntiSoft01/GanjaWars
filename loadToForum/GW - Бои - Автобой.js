74e982ef82cc5bde939bfd82fa890f23ae55d51a
yeugen@gmail.com <yeugen@gmail.com>
2021-01-30 22:07:40 +0100

    Добавлен Автобой 0.2.1

--- loadToForum/GW - Бои - Автобой.js
+++ loadToForum/GW - Бои - Автобой.js
@@ -10,7 +10,6 @@
 // ==/UserScript==
 (() => {
 
-
   const randomDelayFight = generateRandomDelay(1000, 3000)
   const randomDelayPageReload = generateRandomDelay(3001, 5000)
 
@@ -40,43 +39,29 @@
    */
   function getFightMode() {
 
-    let mode = null
+    let mode = document.getElementById('firebutton').className
 
-    if(!!document.getElementById('firebutton')){
-      mode = document.getElementById('firebutton').className
-      if (mode === 'maketurnbutton') { // современное графическое
-        fightIDs = {
-          ...fightIDs, ...{
-            mode: 1,
-            walk: 'walk',
-            grenade: 'bagaboom',
-            activeGeneral: 'apmid',
-            activeWeapon: 'apsid'
-          }
-        }
-      } else if (mode === 'battle_pda_make_turn') { // современное упрощенное
-        fightIDs = {
-          ...fightIDs, ...{
-            mode: 2,
-            walk: 'walkbutton',
-            grenade: 'grenadebutton',
-            activeGeneral: 'apm_button',
-            activeWeapon: 'aps_button'
-          }
-        }
-      }
-    }else{ // старое упрощенное
+    if (mode === 'maketurnbutton') { // современное графическое
       fightIDs = {
         ...fightIDs, ...{
-          mode: 3,
+          mode: 1,
           walk: 'walk',
           grenade: 'bagaboom',
           activeGeneral: 'apmid',
           activeWeapon: 'apsid'
         }
       }
+    } else if (mode === 'battle_pda_make_turn') { // современное упрощенное
+      fightIDs = {
+        ...fightIDs, ...{
+          mode: 2,
+          walk: 'walkbutton',
+          grenade: 'grenadebutton',
+          activeGeneral: 'apm_button',
+          activeWeapon: 'aps_button'
+        }
+      }
     }
-
   }
 
   // Reload current page
@@ -127,62 +112,10 @@
         } catch (err) {}
 
       }
-    } else if (fightIDs.mode === 3) {
-      if (!document.getElementById('euids').innerText.match(/\d+\!/g)) {
-        // если есть достающие цели в бою
-        // использовать активку общую
-        try {document.getElementById(fightIDs.activeGeneral).checked = true} catch (err) {}
-        // использовать активку оружие
-        try {document.getElementById(fightIDs.activeWeapon).checked = true} catch (err) {}
-      } else {
-        // если есть недостающие цели в бою
-        // подходить
-        try {document.getElementById(fightIDs.walk).checked = true} catch (err) {}
-        // кинуть грену
-        try {document.getElementById(fightIDs.grenade).checked = true} catch (err) {}
-      }
     }
 
     fight()
 
   }, randomDelayFight)
-
-
-
-  //
-  //
-  // // if ctrl+Enter are pressed, do the action
-  // window.addEventListener("keydown", (event) => {
-  //   if (event.ctrlKey && event.key === 'Enter') {
-  //     console.log(event)
-  //   }
-  // })
-  //
-  //
-  // // Simulate keyboard keydown
-  // let keyPresser = {};
-  // keyPresser.keydown = (e => {
-  //   var t = document.createEvent("KeyboardEvent");
-  //   Object.defineProperty(t, "keyCode", {
-  //     get() {
-  //       return this.keyCodeVal
-  //     }
-  //   }), Object.defineProperty(t, "which", {
-  //     get() {
-  //       return this.keyCodeVal
-  //     }
-  //   }), t.initKeyboardEvent ? t.initKeyboardEvent("keydown", !0, !0, document.defaultView, e, e, "", "", !1, "") : t.initKeyEvent("keydown", !0, !0, document.defaultView, !1, !1, !1, !1, e, 0), t.keyCodeVal = e, t.keyCode !== e && alert("keyCode mismatch " + t.keyCode + "(" + t.which + ")"), document.body.dispatchEvent(t)
-  // });
-  //
-  // keyPresser.keydown(17)// ctrl
-  // keyPresser.keydown(13)// enter
-  //
-  // // if ctrl+Enter are pressed, do the action
-  // window.addEventListener("keydown", (event) => {
-  //   if (event.ctrlKey && event.key === 'Enter') {
-  //     console.log(event)
-  //   }
-  // })
-
 
 })()
