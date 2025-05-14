5a3681c3973a0782c78875952945597048ab7de7
AntiSoft <yeugen@gmail.com>
2025-05-14 10:44:35 +0200

    Обнова уличные

--- safe/[GWars]streetWar.js
+++ safe/[GWars]streetWar.js
@@ -1,17 +1,14 @@
 // ==UserScript==
-// @name [GWars]streetWar
-// @namespace http://tampermonkey.net/
-// @version 2.0
-// @description Анализ хп и существует ли кнопка создать бой
-// @author       You
+// @name         [GWars]streetWar
+// @namespace    http://tampermonkey.net/
+// @version      2.1
+// @description  Анализ хп и наличие кнопки "Создать заявку"
 // @match        https://www.gwars.io/wargroup.php?war=street*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=gwars.io
 // @grant        none
 // ==/UserScript==
 
 ;(function () {
     const setRedSquareFavicon = () => {
-        // create canvas (red square)
         const canvas = document.createElement('canvas')
         canvas.width = 32
         canvas.height = 32
@@ -22,17 +19,23 @@
 
         const faviconURL = canvas.toDataURL('image/png')
 
-        // delete old favicon
         const oldIcons = document.querySelectorAll(
             'link[rel="icon"], link[rel="shortcut icon"]'
         )
         oldIcons.forEach((icon) => icon.remove())
 
-        // create new favicon
         const link = document.createElement('link')
         link.rel = 'icon'
         link.href = faviconURL
         document.head.appendChild(link)
+    }
+
+    function isCreateBattleButtonVisible() {
+        return [...document.querySelectorAll('a')].some(
+            (a) =>
+                a.href.includes('/wargroup.php?war=street') &&
+                a.textContent.includes('Создать заявку на бой')
+        )
     }
 
     function checkHP() {
@@ -48,20 +51,12 @@
 
         const createButtonExists = isCreateBattleButtonVisible()
 
-        if (percent > 0.8 && createButtonExists) {
+        if (percent >= 0.8 && createButtonExists) {
             setRedSquareFavicon()
         } else {
             console.log('Мало HP или кнопки нет')
         }
     }
 
-    function isCreateBattleButtonVisible() {
-        return (
-            document.querySelector(
-                'a[href*="wargroup.php?war=street&form=1"]'
-            ) !== null
-        )
-    }
-
-    setInterval(checkHP, 2e3)
-})
+    setInterval(checkHP, 1000)
+})()
